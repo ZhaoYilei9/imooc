232b9e242a13eedf0fcb20d9107c1c5426c9905e
ZhaoYilei9 <zyl17763238627@gmail.com>
2018-10-28 12:16:33 +0800

    修复构建bug

--- imoocvideo-dev-common/src/main/java/com/imooc/utils/FetchVideoCover.java
+++ imoocvideo-dev-common/src/main/java/com/imooc/utils/FetchVideoCover.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.util.List;
 
 /**
- * 
+ *
  * @Description: 获取视频的信息
  */
 public class FetchVideoCover {
@@ -18,35 +18,35 @@ public class FetchVideoCover {
 //		ffmpeg.exe -ss 00:00:01 -i spring.mp4 -vframes 1 bb.jpg
 		List<String> command = new java.util.ArrayList<String>();
 		command.add(ffmpegEXE);
-		
+
 		// 指定截取第1秒
 		command.add("-ss");
 		command.add("00:00:01");
-				
+
 		command.add("-y");
 		command.add("-i");
 		command.add(videoInputPath);
-		
+
 		command.add("-vframes");
 		command.add("1");
-		
+
 		command.add(coverOutputPath);
-		
+
 		for (String c : command) {
 			System.out.print(c + " ");
 		}
-		
+
 		ProcessBuilder builder = new ProcessBuilder(command);
 		Process process = builder.start();
-		
+
 		InputStream errorStream = process.getErrorStream();
 		InputStreamReader inputStreamReader = new InputStreamReader(errorStream);
 		BufferedReader br = new BufferedReader(inputStreamReader);
-		
+
 		String line = "";
 		while ( (line = br.readLine()) != null ) {
 		}
-		
+
 		if (br != null) {
 			br.close();
 		}
@@ -73,7 +73,7 @@ public class FetchVideoCover {
 	public FetchVideoCover(String ffmpegEXE) {
 		this.ffmpegEXE = ffmpegEXE;
 	}
-	
+
 	public static void main(String[] args) {
 		// 获取视频信息。
 		FetchVideoCover videoInfo = new FetchVideoCover("c:\\ffmpeg\\bin\\ffmpeg.exe");
